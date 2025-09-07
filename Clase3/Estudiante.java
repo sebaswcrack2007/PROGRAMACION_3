83cb80218863ab31c05d7d19a39cc1717f8eb487
sebaswcrack2007 <sebaswcrack2007@gmail.com>
2025-09-07 18:37:48 +0000

    confirma

--- Clase3/Estudiante.java
+++ Clase3/Estudiante.java
@@ -1,29 +1,34 @@
-public class EjecutarEstudiante {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
-        
-        Estudiante objEst1 = new Estudiante(1, 1006720569, "Angelica", "Ingeniería");
-        Estudiante objEst2 = new Estudiante(2, 1001256512, "Miguel", "Ingeniería");
-        Estudiante objEst3 = new Estudiante(3, 1000999111, "Byron", "Medicina");
+public class Estudiante {
+    
+    private int id;
+    private int cedula;
+    private String nombre;
+    private String facultad;
+    private String[] cursos; // Nuevo atributo
 
-       
-        Estudiante[] e = new Estudiante[4];
-        e[0] = objEst1;
-        e[1] = objEst2;
-        e[2] = objEst3;
-        e[3] = new Estudiante(4, 1008940941, "Peter", "Ingeniería");
+   
+    public Estudiante(int id, int cedula, String nombre, String facultad) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.facultad = facultad;
+        this.cursos = new String[0]; 
+    }
 
-        // === Actividad independiente ===
-        
-        objEst1.matricularCursos(new String[] { "Programación", "Matemáticas", "Bases de Datos" });
-        objEst2.matricularCursos(new String[] { "Física", "Cálculo", "Algoritmos" });
-        objEst3.matricularCursos(new String[] { "Anatomía", "Biología", "Química" });
-        e[3].matricularCursos(new String[] { "Ética", "Redes", "Inteligencia Artificial" });
+    
+    public void matricularCursos(String[] cursos) {
+        this.cursos = cursos; 
+        System.out.println("Cursos matriculados por " + nombre + ": " + Arrays.toString(cursos));
+    }
 
-        
-        System.out.println("\n--- Lista de estudiantes con sus cursos ---");
-        for (Estudiante est : e) {
-            System.out.println(est);
-        }
+    // Método toString
+    public String toString() {
+        return "Estudiante id: " + id 
+            + " | Cédula: " + cedula 
+            + " | Nombre: " + nombre 
+            + " | Facultad: " + facultad 
+            + " | Cursos: " + Arrays.toString(cursos) + " ]";
     }
 }
