10a55dbbb3adff313e770c5a1c6bc4566cd35161
sebaswcrack2007 <sebaswcrack2007@gmail.com>
2025-09-16 16:07:17 +0000

    Pilas

---
+++ Clase7/Pilas.java
@@ -0,0 +1,33 @@
+package Clase7;
+
+import java.util.Stack;
+
+public class Pilas{
+    public static void main(String[] args) {
+
+        //Creacion de una pila de numeros enteros
+        Stack<Integer> pila = new Stack<>();
+
+        //Insertar elementos en la pila
+        pila.push (8);
+        pila.push (4);
+        pila.push (9);  
+        pila.push (10);
+        pila.push (6);
+
+        //Imprimir la pila
+
+        System.out.println(pila);
+
+        //Mostrar el elemento que esta en el tope de la pila sin removerlo
+        System.out.println("Tope de la pila: " + pila.peek());
+
+        //Remover el elemento que se encuentra en el tope de la pila
+        System.out.println("Elemento removido del tope de la pila: " + pila.pop());
+
+        //Imprimir la pila
+
+        System.out.println(pila);
+    }
+
+}
